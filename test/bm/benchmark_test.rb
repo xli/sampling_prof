29da4fa192074fa2e71b44638479ab754358a430
Xiao Li <swing1979@gmail.com>
2014-03-27 09:05:18 -0700

    add benchmark for multithreading mode

--- test/bm/benchmark_test.rb
+++ test/bm/benchmark_test.rb
@@ -2,24 +2,53 @@ require 'test_helper'
 require 'benchmark'
 
 class BenchmarkTest < Test::Unit::TestCase
-  def setup
-    @prof = SamplingProf.new(0.01)
-  end
-
   def test_profile_and_output_text_result
+    @prof = SamplingProf.new(0.01)
     t = 40
     puts "t: #{t}"
     Benchmark.bm do |x|
-      x.report('warm up') do
+      x.report('warm up        ') do
         5.times { my_fib(t) }
       end
-      x.report('no profiling') do
+      x.report('no profiling   ') do
         my_fib(t)
       end
-      x.report('with profiling') do
+      x.report('with profiling ') do
         @prof.profile { my_fib(t) }
       end
     end
+  end
+
+  def test_multithreading_profiling
+    @prof = SamplingProf.new(0.1, true, 1) do |data|
+      # do nothing
+    end
+    t = 40
+    tc = 16
+    puts "t: #{t}, thread count: #{tc}"
+    Benchmark.bm do |x|
+      x.report('warm up        ') do
+        5.times { my_fib(t) }
+      end
+      x.report('no profiling   ') do
+        threads = (1..tc).map do |i|
+          Thread.start do
+            my_fib(t)
+          end
+        end
+        threads.each(&:join)
+      end
+      x.report('with profiling ') do
+        threads = (1..tc).map do |i|
+          Thread.start do
+            @prof.profile { my_fib(t) }
+          end
+        end
+        threads.each(&:join)
+      end
+    end
+  ensure
+    @prof.terminate
   end
 
   def my_fib(i)
